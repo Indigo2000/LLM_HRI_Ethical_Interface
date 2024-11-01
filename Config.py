66667efd149fdbef3f641a2bdad16e52ab23bd88
Indigo2000 <105358399+Indigo2000@users.noreply.github.com>
2024-11-01 14:16:05 +0000

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,5 +1,11 @@
-import json
+import sys
 import os
+# Make sure to look in the local path for the libraries
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'langchain_core'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'langchain_openai'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'openai'))
+
+import json
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 
