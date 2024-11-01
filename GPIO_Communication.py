0c2a057463c75f0079c7d560d55f4418e5e0d782
Indigo2000 <105358399+indigo2000@users.noreply.github.com>
2024-11-01 14:17:26 +0000

    Update GPIO_Communication.py

--- GPIO_Communication.py
+++ GPIO_Communication.py
@@ -1,35 +1,49 @@
-from gpiozero import Motor, Device
-from gpiozero.pins.pigpio import PiGPIOFactory
+try:
+    from gpiozero import Motor, Device
+    from gpiozero.pins.pigpio import PiGPIOFactory
+    motors = True
+except ModuleNotFoundError:
+    print("gpiozero module not found - motor action will not be simulated")
+    motors = False
+
 import asyncio
 
-Device.pin_factory = PiGPIOFactory()
+if motors:
+
+    Device.pin_factory = PiGPIOFactory()
 
 # GPIO setup
-motor_fb = Motor(forward=12, backward=13)
-motor_lr = Motor(forward=18, backward=19)
+    motor_fb = Motor(forward=12, backward=13)
+    motor_lr = Motor(forward=18, backward=19)
 
 # Motor control functions
 def motor_stop():
-    motor_fb.stop()
-    motor_lr.stop()
+    if motors:
+        motor_fb.stop()
+        motor_lr.stop()
 
 async def motor_forward(duration):
-    motor_fb.forward()
+    if motors:
+        motor_fb.forward()
     await asyncio.sleep(duration)
 
 async def motor_backward(duration):
-    motor_fb.backward()
+    if motors:
+        motor_fb.backward()
     await asyncio.sleep(duration)
 
 async def motor_left(duration):
-    motor_lr.forward()
+    if motors:
+        motor_lr.forward()
     await asyncio.sleep(duration)
 
 async def motor_right(duration):
-    motor_lr.backward()
+    if motors:
+        motor_lr.backward()
     await asyncio.sleep(duration)
 
 #Turn off any running motors
-motor_stop
+if motors:
+    motor_stop
     
 
