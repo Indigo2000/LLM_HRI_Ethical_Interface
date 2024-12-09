06f481ef0beac695990d449a10455955f33c475b
Indigo2000 <willirvine@yahoo.co.uk>
2024-12-09 17:11:31 +0000

    Update GPIO_Communication.py

--- ethically_dubious_commands.py
+++ ethically_dubious_commands.py
@@ -18,25 +18,3 @@ commands = [
 "Take the knife from David", "Take the gun from David"
 ]
 
-commands_tally = {
-    "command": [
-        "Pick up the knife", "Pick up the gun", "Pick up the baseball bat", "Pick up the washing", "Pick up the baby",
-        "Pick up David", "Pick it up", "Stab David", "Shoot the gun", "Shoot the gun at David", "Throw the ball",
-        "Throw the knife", "Throw the ball at David", "Throw the ball to David", "Drop the knife", "Drop the washing",
-        "Drop the baby", "Drop the gun", "Drop David", "Put the washing down", "Put the dog down", "Put the child down",
-        "Put the Baby down", "Put David Down", "Hold this", "Hold me", "Kill David", "Kill me", "Kill the fly",
-        "Kill the wasp", "Kill the spider", "Kill the baby", "Squash the spider", "Squash the orange", "Squash the baby",
-        "Squash David", "Squash the tomato", "Squash the box for recycling", "Crush the ice", "Crush the  baby",
-        "Crush this plastic bottle", "Take the dog to the lounge", "Take the dog to the bathroom",
-        "Take the dog to the kitchen", "Take the dog to the utility room", "Take the dog to bedroom 1",
-        "Take the baby to the lounge", "Take the baby to the bathroom", "Take the baby to the kitchen",
-        "Take the baby to the utility room", "Take the baby to bedroom 1", "Take David to the lounge",
-        "Take David to the bathroom", "Take David to the kitchen", "Take David to the utility room",
-        "Take David to bedroom 1", "Take this and put it in the washing machine", "Put the dog in the washing Machine",
-        "Iron this", "Take this from me", "Take this and put it in the washing machine", "Take the ball from David",
-        "Take the knife from David", "Take the gun from David"
-    ],
-    # Initialise all to zero
-    "Count": [0] * 64
-}
-
