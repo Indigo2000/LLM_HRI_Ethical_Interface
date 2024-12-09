e8bae2dd3b8de10daf8b9f13dfea760854d290d8
Indigo2000 <willirvine@yahoo.co.uk>
2024-12-09 15:11:03 +0000

    Update GPIO_Communication.py

--- Analysis.py
+++ Analysis.py
@@ -38,6 +38,19 @@ two_commands_one_line = 0
 total_goals_identified = 0
 already_there = 0
 location_unidentified = 0
+correct_queue = 0
+queue_counter = 0
+# For checking which line phrase appears in file
+start_line = 0
+# Lists of dictionaries for commands and goals
+queued_goals = []
+inferred_goals = []
+a_star_route_check = []
+# For counting entries in the list
+list_entry = 0
+# For counting entries in A Star route
+a_star_route_count = 0
+
 
 # Function to update tallies in dataframes
 def tally_update(current_line, to_tally):
@@ -94,6 +107,9 @@ with open(file_standard, 'r') as standard_file:
             # Count how many times a location appears in an instruction
             location_in_command_count = location_in_command_count + tally_update(lower_line, df_command_locations)
 
+            # Add key for checking queue operation
+            queued_goals.append({"Given Command": lower_line[39:-1], "Processed Command": ""})
+
         # Check for ethical issues found then retrieve locations
         if "was ethical issue found with command:" in lower_line:
             ethical_issues += 1
@@ -101,10 +117,17 @@ with open(file_standard, 'r') as standard_file:
             # Count how many times a location appears in an instruction
             location_in_command_count = location_in_command_count + tally_update(lower_line, df_command_locations)
 
+        if "start location is:" in lower_line:
+            # Add start for checking A* operation
+            a_star_route_check.append({"Start": lower_line[20:-1], "Goal": "", "Route": ""})
+
         # Tally up goal locations identified
         if "goal location is:" in lower_line:
             tally_update(lower_line, df_goal_locations)
             total_goals_identified += 1
+
+            # Add in the goal for this command to A* route check
+            a_star_route_check[a_star_route_count]["Goal"] = lower_line[19:-1]
 
         # Update location not identified
         if "location not identified!" in lower_line:
@@ -115,18 +138,34 @@ with open(file_standard, 'r') as standard_file:
         if "already at location" in lower_line:
             already_there += 1
 
+        # Check the queue for correct operation - does processing match given?
+        if "processing command" in lower_line and "finished" not in lower_line:
+            # Find the next entry in the list and update its value
+            queued_goals[list_entry]["Processed Command"] = lower_line[19:-1]
+            list_entry += 1
 
+        # Add route to A* route check
+        if "route found to be:" in lower_line:
+            a_star_route_check[a_star_route_count]["Route"] = lower_line[21:-2]
+            a_star_route_count +=1
+
+# Convert queued goals data to a data frame
+df_queued_goals = pd.DataFrame(queued_goals)
+
+# Convert A* route check to a data frame
+df_a_star_route_check = pd.DataFrame(a_star_route_check)
+
+# Check queue for correct operation
+for item in queued_goals:
+    if queued_goals[queue_counter]["Given Command"][:-1] == queued_goals[queue_counter]["Processed Command"][2:]:
+        correct_queue += 1
+    queue_counter += 1
 ###### Now check commands for inferring location
 
-# For checking which line phrase appears in file
-start_line = 0
-
-# List of dictionaries for command and goal
-inferred_goals = []
-
-# For counting entries in the list
+# Reset required counters
 list_entry = 0
-# Read the file for standard commands and count occurrences
+
+# Read the file for inferred commands and count occurrences
 with open(file_inferred, 'r') as inferred_file:
     content = inferred_file.readlines()
 
@@ -135,11 +174,10 @@ with open(file_inferred, 'r') as inferred_file:
         lower_line = line.lower()
 
         if "processing command" in lower_line and not "finished" in lower_line:
-            start_line = count
-            inferred_goals.append({"key": lower_line[:-1], "value": str(start_line)})
+            inferred_goals.append({"Command": lower_line[21:-1], "Goal": ""})
         if "goal location is:" in lower_line:
             # Find the next entry in the list and update it's value
-            inferred_goals[list_entry]["value"] = lower_line[:-1]
+            inferred_goals[list_entry]["Goal"] = lower_line[19:-1]
             list_entry += 1
 
 # Convert to a dat frame
@@ -176,6 +214,13 @@ print("\n")
 print("Total number of times robot already there: ", already_there, " out of 360 times.\n")
 print("\n")
 print("Total number of times location not identified: ", location_unidentified, " out of 360 times.\n")
+print("\n\n")
+print("Queue operation - command given then command processing:")
+print(df_queued_goals.to_string(justify="left"))
+print("\n", correct_queue, " out of 360 commands queued and processed correctly")
+print("\n\n")
+print("A* route check:")
+print(df_a_star_route_check.to_string(justify="left"))
 print("\n\n")
 print("Inferred Commands:")
 print(df_inferred_locations.to_string(justify="left"))
