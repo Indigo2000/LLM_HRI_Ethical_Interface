c966b607d142c9dc4ee02753d2a3dee5b1f97127
Indigo2000 <willirvine@yahoo.co.uk>
2024-12-17 08:30:38 +0000

    Update GPIO_Communication.py

--- layout.py
+++ layout.py
@@ -111,10 +111,10 @@ async def a_star_search(graph, start, goal, h):
             if neighbour in closed_set:
                 continue
             # If neighbour not yet explored, calculate new f(n) and update path
-            tentative_g_score = g_score + cost
-            tentative_f_score = tentative_g_score + h[neighbour] # A* calculation of cost f(n) = g(n) + h(n)
+            updated_g_score = g_score + cost
+            updated_f_score = updated_g_score + h[neighbour] # A* calculation of cost f(n) = g(n) + h(n)
             new_path = path + [(neighbour)]
             # Push updated costs, and path to open set for this node (contained in variable neighbour)
-            heapq.heappush(open_set, (tentative_f_score, tentative_g_score, neighbour, new_path))
+            heapq.heappush(open_set, (updated_f_score, updated_g_score, neighbour, new_path))
 
     return None
